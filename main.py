aaa8f7073d88d1104007d2c5cde611f5b2a32ac8
YCR160 <3342711246@qq.com>
2023-07-12 21:54:40 +0800

    add detection

--- main.py
+++ main.py
@@ -8,8 +8,16 @@ from scripts.getConfig import getConfig
 from scripts.Main import Main
 import cv2
 
+# 以下代码引入PaddleDetection模块
+import sys
+sys.path.append('../PaddleLiteDemo/Python/')
+import detection as Det
+import time
 
-def work(DealFlag, ImgQueue):
+# shared_capture = cv2.VideoCapture(0)
+# from scripts.Main import Main
+
+def work(DealFlag, ImgQueue, Frame):
     """
     主运行进程
     :param DealFlag: 共享内存变量，用于标记是否有图像可处理
@@ -19,7 +27,7 @@ def work(DealFlag, ImgQueue):
     # 获取当前脚本文件的绝对路径
     full_path = os.path.dirname(os.path.realpath(__file__))
     Config = getConfig(full_path)
-    img_processor = Main(Config, DealFlag, ImgQueue)
+    img_processor = Main(Config, DealFlag, ImgQueue, Frame)
     # 不断从 ImgQueue 中获取待处理的图像，并调用 Main 对象的方法进行处理，直到 DealFlag 为 0
     img_processor.mainloop()
 
@@ -40,14 +48,72 @@ def show(DealFlag, ImgQueue):
             # 等待 5 毫秒，以允许窗口显示图像
             cv2.waitKey(5)
 
+def detection(Frame):
+    system_config_path = "../PaddleLiteDemo/configs/detection/ssd_mobilenet_v1/usbcamera.json"
+    print("SystemConfig Path: {}".format(system_config_path))
+    Det.g_system_config = Det.SystemConfig(system_config_path)
+    model_config_path = Det.g_system_config.model_config_path
+    system_config_root = system_config_path[:system_config_path.rfind("/")]
+    Det.g_model_config = Det.ModelConfig(os.path.join(system_config_root, model_config_path))
+
+    display = Det.Display("PaddleLiteDetectionDemo")
+    timer = Det.Timer("Predict", 100)
+    capture = Det.createCapture(Det.g_system_config.input_type, Det.g_system_config.input_path)
+    # capture.cap = shared_capture
+    if capture is None:
+        exit(-1)
+
+    ret = Det.predictorInit()
+    if ret != 0:
+        print("Error!!! predictor init failed .")
+        sys.exit(-1)
+
+    while True:
+        frame = capture.getFrame()
+        Frame.put(frame)
+        # print(len(frame[0]))
+        origin_frame = frame.copy()
+        start = time.time()
+        predict_result = Det.predict(frame, timer)
+        end = time.time()
+        # print("result_len", len(predict_result))
+        # for result in predict_result:
+        #     print("result_index: ", result.type)
+        #     print("result_x: ", result.x)
+        #     print("result_y: ", result.y)
+        #     print("result_h: ", result.height)
+        #     print("result_w: ", result.width)
+        # print("result",predict_result, end-start)
+        # print("frame_size: ", origin_frame.size)
+        # Det.drawResults(origin_frame, predict_result)
+        if Det.g_system_config.predict_log_enable:
+            Det.printResults(origin_frame, predict_result)
+
+        # if Det.g_system_config.predict_log_enable and capture.getType() != "image":
+        #     display.putFrame(origin_frame)
+        # if capture.getType() == "image":
+        #     cv2.imwrite("DetectionResult.jpg", origin_frame)
+        #     break
+
+        # if Det.g_system_config.predict_time_log_enable:
+        #     timer.printAverageRunTime()
+
+    if Det.g_system_config.display_enable and Det.g_system_config.input_type != "image":
+        display.stop()
+
+    capture.stop()
+    return
+
 
 if __name__ == "__main__":
     ImgQueue = mp.Queue()           # 用于存储待处理的图像，先进先出队列，实现不同进程数据交互
     DealFlag = mp.Value('B', 0)     # DealFlag为 1 则表示有待处理图片，为 0 则表示没有待处理图片
+    Frame = mp.Queue()              # 用于存储原始图像，实现不同进程数据交互
     Mps = []                        # 用于存储多个进程对象
 
-    Mps.append(mp.Process(target=work, args=(DealFlag, ImgQueue)))
+    Mps.append(mp.Process(target=work, args=(DealFlag, ImgQueue, Frame)))
     Mps.append(mp.Process(target=show, args=(DealFlag, ImgQueue)))
+    Mps.append(mp.Process(target=detection, args=(Frame,)))
 
     # 列表推导式启动 Mps 列表中的所有进程
     [Mp.start() for Mp in Mps]
@@ -55,3 +121,5 @@ if __name__ == "__main__":
     Mps[0].join()
     # 列表推导式终止 Mps 列表中的所有进程
     [Mp.terminate() for Mp in Mps]
+
+    # shared_capture.release()
--- scripts/Main.py
+++ scripts/Main.py
@@ -5,85 +5,52 @@ import cv2
 import time
 import numpy as np
 import multiprocessing as mp
-# import matplotlib
-# matplotlib.use('TkAgg')
-import matplotlib.pyplot as plt
 from Config import *
 
-capture = cv2.VideoCapture(0)
-# print(capture.get(cv2.CAP_PROP_FRAME_HEIGHT))
-capture.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'))
-capture.set(cv2.CAP_PROP_FRAME_WIDTH, 320)
-capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 240)
-capture.set(cv2.CAP_PROP_CONTRAST, 30)  # 30 对比度
-capture.set(cv2.CAP_PROP_BRIGHTNESS, 20)  # 20 亮度
-# 通过调整对比度和亮度，让图像能够稳定下来，不会在运行过程中因为亮度变化而导致鲁棒性变差
-# print(capture.get(cv2.CAP_PROP_FRAME_WIDTH))
-# print(capture.get(cv2.CAP_PROP_FRAME_HEIGHT))
-fourcc = cv2.VideoWriter_fourcc(*'XVID')
-out = cv2.VideoWriter('output.avi', fourcc, 30.0, (176, 80))
+# capture = cv2.VideoCapture(0)
+# # print(capture.get(cv2.CAP_PROP_FRAME_HEIGHT))
+# capture.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'))
+# capture.set(cv2.CAP_PROP_FRAME_WIDTH, 320)
+# capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 240)
+# capture.set(cv2.CAP_PROP_CONTRAST, 30)  # 30 对比度
+# capture.set(cv2.CAP_PROP_BRIGHTNESS, 20)  # 20 亮度
+# # 通过调整对比度和亮度，让图像能够稳定下来，不会在运行过程中因为亮度变化而导致鲁棒性变差
+# # print(capture.get(cv2.CAP_PROP_FRAME_WIDTH))
+# # print(capture.get(cv2.CAP_PROP_FRAME_HEIGHT))
+# fourcc = cv2.VideoWriter_fourcc(*'XVID')
+# out = cv2.VideoWriter('output.avi', fourcc, 30.0, (176, 80))
 
 
 class Main:
     # def show(self, DealFlag, ImgQueue):
 
-    def __init__(self, Config, DealFlag, ImgQueue) -> None:
+    def __init__(self, Config, DealFlag, ImgQueue, Frame) -> None:
         from .ImgWindow import ImgWindow
-        # a = []
-        # b = []
-        lastwS = 0
         # 帧大小缩小到宽度为 176 像素，高度为 80 像素
         down_width, down_height = 176, 80
         down_points = (down_width, down_height)
 
-        while True:
-            ret, frame = capture.read()
-            if not ret:
-                break
+        alpha = 1.5  # 对比度调整系数
+        beta = 30  # 亮度调整系数
 
+        while True:
+            # ret, frame = capture.read()
+            # if not ret:
+            #     break
+            frame = 0
+            frame_flag = 0
+            while frame_flag == 0:
+                while not Frame.empty():
+                    frame = Frame.get()
+                    frame_flag = 1
+
+            frame = cv2.convertScaleAbs(frame, alpha=alpha, beta=beta) # 调整亮度和对比度
+            frame = np.array(frame)
             frame = cv2.resize(frame, down_points,
                                interpolation=cv2.INTER_LINEAR)
-            # hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)    # 使用 HSV 颜色空间
-
-            # lower_red = np.array([160, 70, 81])             # 设置需要识别的车道线颜色的阈值
-            # upper_red = np.array([179, 255, 255])
-
-            # lower_red2 = np.array([0, 70, 81])              # 设置需要识别的车道线颜色的阈值
-            # upper_red2 = np.array([20, 255, 255])
-            # dark_blue = np.uint8([[[12,22,121]]])
-            # dark_blue = cv2.cvtColor(dark_blue,cv2.COLOR_BGR2HSV)
-
-            # mask = cv2.inRange(hsv, lower_red, upper_red) + \
-                # cv2.inRange(hsv, lower_red2, upper_red2)    # 提取所需颜色
-            # 腐蚀膨胀操作，去除噪声和不必要的细节
-            # erode_kernel = np.ones((1, 3), np.uint8)
-            # mask = cv2.erode(mask, erode_kernel)
-            # dilate_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (37, 10))
-            # mask = cv2.dilate(mask, dilate_kernel)
-            # 在图像的第 50 行到第 79 行之间，提取的颜色在图像中的像素点数量
-            # countRed = np.count_nonzero(mask[50:79, :])
-            # countRed2 = np.count_nonzero(mask)
-            # print("reddddddddddddddddddddddddddd",
-            #       countRed)                                 # 此处是为了识别赛道中的红色元素写的红色判断
-            # nonzero = np.nonzero(mask)
-            # x = nonzero[0]
-            # y = nonzero[1]
-            # if countRed2 > 50:
-            #     frame[nonzero] = [220, 220, 220]  # 这里将赛道中心的红色标志给去掉，防止干扰后面的赛道识别
-            # fcolor1 = frame[79, 87]
-            # fcolor2 = frame[60, 87]
-            # fcolor1 = np.array(fcolor1)
-            # fcolor2 = np.array(fcolor2)
-            # aerial = cv2.warpPerspective(frame, H, (200, 200)) # 对视频帧进行透视变换，将其转换为鸟瞰图
-            # cv2.imshow('g',frame)
-            # cv2.waitKey(1)
 
             self.Config = Config
             self.imgWindow = ImgWindow(self)
-            # if lastwS >= 2:
-            #    frame[:,:,0] = frame[:,:,0] & mask
-            #    frame[:,:,1] = frame[:,:,1] & mask
-            #    frame[:,:,2] = frame[:,:,2] & mask
             frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) # 将图像转换为灰度图
             self.imgWindow.setImg(frame) # 将图像传入 ImgWindow
             self.imgWindow.imgProcess.work(0, 0, 0, 0) # 调用 ImgProcess 的 work 函数
@@ -91,9 +58,6 @@ class Main:
             #     fcolor1, fcolor2, countRed, countRed2) # 调用 ImgProcess 的 work 函数
 
             # 后面是一些调试输出
-            # lastwS = self.imgWindow.imgProcess.wS
-            # a.append(self.imgWindow.imgProcess.landmark["Yaw"])
-            # b.append(self.imgWindow.imgProcess.pred)
             output = self.imgWindow.imgProcess.canny2
             color = (0, 0, 255) # 蓝色
             # print("F========================", self.imgWindow.imgProcess.F)
@@ -110,7 +74,6 @@ class Main:
             # 在图像上绘制线段
             cv2.line(output, (176 // 2 - 1, 80 - 1), (round(176 // 2 - 1 +
                      40 * self.imgWindow.imgProcess.landmark["Yaw"]), 80-40), color, 2)
-
             # 用于将处理后的视频帧存储在一个队列中，并将队列交给另一个进程进行输出
             # 如果共享变量 DealFlag 为 0
             if not DealFlag.value:
@@ -118,7 +81,6 @@ class Main:
                 while not ImgQueue.empty():
                     ImgQueue.get()
                 ImgQueue.put(output)
-                # ImgQueue.put(aerial)
                 DealFlag.value = 1
 
             # cv2.imshow('2',self.imgWindow.imgProcess.PERMAT)
@@ -130,8 +92,5 @@ class Main:
             # if cv2.waitKey(2) == ord('q'):
             #    break
 
-        # plt.plot(a,color="b")  #真实值
-        # plt.plot(b,color="g")  #测量值
-        # plt.show()
-        capture.release()
+        # capture.release()
         # out.release()
